b22e2e37f5ccc30e9a411bfea29772e17fe76c1c
Pavan Preetham Mukkunda <pavanpreetham694@gmail.com>
2026-09-02 21:48:41 +0530

    Add apify connection test - connetion works

---
+++ test_apify.py
@@ -0,0 +1,8 @@
+from apify_client import ApifyClient
+import config
+
+client = ApifyClient(config.APIFY_TOKEN)
+
+user_info = client.user().get()
+print("Connected as:", user_info.username)
+print("Account email:", user_info.email)
